18af29d3cd63728d417d5f11b973e69c43bf7be8
Qrhl <quentin.ruhl27@gmail.com>
2018-06-25 16:02:16 +0800

    Modification ConfReader.py, ReadMe.md & test.conf

--- ConfReader.py
+++ ConfReader.py
@@ -1,5 +1,6 @@
 import os
 
+
 class ConfigKeyException(Exception):
     """
     Exception that is raised if the key is not in the dictionary thus not in the configuration file.
@@ -14,10 +15,10 @@ class ConfigKeyException(Exception):
 class ConfReader:
     """
     This class is a parser for a configuration file. It reads the keys and values of the file and write them in a dictionary.
-    The configuration file must be written as follows: KEY = Value and a line unused or a commentary must strart with a #
+    The configuration file must be written as follows: KEY = Value and a line unused or a commentary must start with a #
     """
 
-    def __init__(self, filename, path='./'):
+    def __init__(self, filename, environment='', path='./'):
         """
         Simple constructor for the object
         :param filename: name of the configuration file
@@ -25,28 +26,78 @@ class ConfReader:
         """
         self.path = os.path.join(path, filename)
         self.conf = {}
+        self.environment = environment
+        self.parse_env()
         self.parse_values()
+
+    def parse_env(self):
+        """
+        This method reads the configuration file and recovers the environment to use if not specified
+        :return: /
+        """
+        if self.environment == '':
+            with open(self.path) as conf_file:
+                lines = conf_file.readlines()
+                for line_conf in lines:
+                    line_conf.strip('\n').strip('\r').strip()
+                    if not line_conf.startswith("#") and line_conf != '\r' and line_conf != '\n' and line_conf != ' ':
+                        try:
+                            conf_key = line_conf.split("=")[0].strip('\n').strip("\r").strip()
+                            conf_value = line_conf.split("=")[1].strip('\n').strip("\r").strip()
+                            if conf_key == 'ENV':
+                                self.environment = conf_value
+                        except IndexError:
+                            pass
 
     def parse_values(self):
         """
         This method reads the configuration file and stores the data in a dictionary
-        :return:
+        :return: /
         """
         with open(self.path) as conf_file:
             lines = conf_file.readlines()
+            current_env = ''
+            empty = 0
             for line_conf in lines:
-                if not line_conf.startswith("#"):
-                    self.conf[line_conf.split("=")[0].strip('\n').strip("\r").strip()] = line_conf.split("=")[1].strip('\n').strip("\r").strip()
-        return True
+                line_conf.strip('\n').strip('\r').strip()
+                if not line_conf.startswith("#") and line_conf != '\r' and line_conf != '\n' and line_conf != ' ':
+                    if self.environment != '':
+                        try:
+                            if line_conf.find('[') != -1 or line_conf.find(']') != -1:
+                                current_env = line_conf.split('[')[1].split(']')[0]
+                            else:
+                                if current_env == self.environment:
+                                    conf_key = line_conf.split("=")[0].strip('\n').strip("\r").strip()
+                                    conf_value = line_conf.split("=")[1].strip('\n').strip("\r").strip()
+                                    if conf_key not in self.conf.keys() and conf_key != "ENV":
+                                        self.conf[conf_key] = conf_value
+                                        if conf_value == '':
+                                            empty += 1
+                        except IndexError as e:
+                            print('\x1b[31;0m' + e.__str__() + '\x1b[0m')
+                            print("The configuration file might have some issues. Check it.")
+                    else:
+                        try:
+                            conf_key = line_conf.split("=")[0].strip('\n').strip("\r").strip()
+                            conf_value = line_conf.split("=")[1].strip('\n').strip("\r").strip()
+                            if conf_key not in self.conf.keys() and conf_key != "ENV":
+                                self.conf[conf_key] = conf_value
+                                if conf_value == '':
+                                    empty += 1
+                        except IndexError:
+                            print('\x1b[31;0m' + e.__str__() + '\x1b[0m')
+                            print("The configuration file might have some issues. Check it.")
+            if empty > 0:
+                print('\x1b[34;0m' + "There is {} empty value(s) in the config file!".format(empty) + '\x1b[0m')
 
     def get_value(self, key):
         """
         :param key: Item of the configuration file
         :return: the value associated to 'key' in the config file
         """
-        try:
-            return self.conf[key]
-        except KeyError:
-            raise ConfigKeyException
-        
+        return self.conf[key]
 
+
+if __name__ == "__main__":
+    config = ConfReader("/Users/quentin/PycharmProjects/ConfReader/test.conf")
+    print(config.conf)
--- README.md
+++ README.md
@@ -1,3 +1,31 @@
 # ConfReader
 
-This is a class that reads a configuration file and allows the user to get the values from the reader instead of hardcoding them in their code.
+Configuration files are files used to configure the parameters and initial settings used in scripts without hardcoding them.
+
+To write a configuration file, the rules are the following :
+- It must be "key" = "value" in the file, one per line only
+```
+login = mylogin
+password = mypassword
+```
+- At the beginning of the file, you can put an Environment value "ENV" that enables you to have a different sets of parameters.
+For example, the following file will return "mytest" for the key "login". You can also setup different values in the different environment.
+You must only remember that it will ONLY take the values between [test] and the next [] or the end.  
+
+```
+ENV = test
+
+[test]
+
+login = mytest
+
+[prod]
+
+login = myprod
+```
+- The usage is the following:
+```python
+from Classes_SG.conf.ConfReader import *
+config = ConfReader("my_config_file.conf", path=path_conf)
+login = config.get_value("login")
+```
